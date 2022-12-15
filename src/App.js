56acb3cc320062b595ecf4f529148aa889f45e3a
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2022-12-15 23:14:45 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import Refresh from "./refresh.png";
+
+
 
 function App() {
+
+  
+  const [easy, setEasy] = useState(Math.floor(Math.random()*101));
+  function handleClickEasy(value) {
+    if (value > 100 || value < 0) {
+      alert("Enter value between 0-100");
+    } else {
+      console.log(value);
+      setEasy(2 * value);
+    }
+  }
+
+  const [med, setMed] = useState(Math.floor(Math.random()*101));
+  function handleClickMed(value) {
+    if (value > 100 || value < 0) {
+      alert("Enter value between 0-100");
+    } else {
+      console.log(value);
+      setMed(2 * value);
+    }
+  }
+
+  const [hard, setHard] = useState(Math.floor(Math.random()*101));
+  function handleClickHard(value) {
+    if (value > 100 || value < 0) {
+      alert("Enter value between 0-100");
+    } else {
+      console.log(value);
+      setHard(2 * value);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body id="mainBody">
+        <div className="easy">
+          <p>{easy / 2}% </p>
+          <div id="bar-container">
+            <motion.div
+              id="bar"
+              animate={{
+                height: `${easy}px`,
+              }}
+              transition={{
+                duration: 1,
+              }}
+            ></motion.div>
+          </div>
+          <p>Easy</p>
+          <input id="input" type="number"></input>
+          <button
+            id="refreshBtn"
+            onClick={() =>
+              handleClickEasy(document.getElementById("input").value)
+            }
+          >
+            <img src={Refresh}></img>
+          </button>
+        </div>
+
+        <div className="medium">
+          <p>{med / 2}% </p>
+          <div id="bar-container">
+            <motion.div
+              id="bar2"
+              animate={{
+                height: `${med}px`,
+              }}
+              transition={{
+                duration: 1,
+              }}
+            ></motion.div>
+          </div>
+          <p>Medium</p>
+          <input id="input2" type="number"></input>
+
+          <button
+            id="refreshBtn"
+            onClick={() =>
+              handleClickMed(document.getElementById("input2").value)
+            }
+          >
+            <img src={Refresh}></img>
+          </button>
+        </div>
+
+        <div className="hard">
+          <p>{hard / 2}% </p>
+          <div id="bar-container">
+            <motion.div
+              id="bar3"
+              animate={{
+                height: `${hard}px`,
+              }}
+              transition={{
+                duration: 1,
+              }}
+            ></motion.div>
+          </div>
+          <p>Hard</p>
+          <input id="input3" type="number"></input>
+          <button
+            id="refreshBtn"
+            onClick={() =>
+              handleClickHard(document.getElementById("input3").value)
+            }
+          >
+            <img src={Refresh}></img>
+          </button>
+        </div>
+      </body>
     </div>
   );
 }
